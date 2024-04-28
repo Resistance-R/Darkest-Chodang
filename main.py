0b9b771fc0d2497afe062ffa273c9c97a7b2b0f9
resistancer <auddbswo28@gmail.com>
2024-04-28 18:32:17 +0900

    Implementing function Btn in class ControlGamepad

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import pygame
+import evdev
+import time
 
-print("Hello, World!")
+class ControlGamepad():
+    def __init__(self, BUTTON, TRIGGER, STICK):
+        self.Button = BUTTON
+        self.Trigger = TRIGGER
+        self.Stick = STICK
+
+    def Btn(self, BUTTON_KEY, joystick):
+        joystick.set_button(BUTTON_KEY, 1)
+        time.sleep(0.1)
+        joystick.set_button(BUTTON_KEY, 0)
+
+
+pygame.init()
+pygame.joystick.init()
+
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
