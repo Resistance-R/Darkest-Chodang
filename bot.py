8baf9689af527ee6a371cbff260c4599bb39e9a3
resistancer <auddbswo28@gmail.com>
2024-07-01 13:03:38 +0900

    add command counter, and exeption handling

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import my_gamepad
 import my_token
 
 TOKEN = my_token.myToken
-CHENNAL_ID = my_token.myChennal
+CHENNAL_ID = my_token.myChannel
 
 A_BTN = vg.XUSB_BUTTON.XUSB_GAMEPAD_A
 B_BTN = vg.XUSB_BUTTON.XUSB_GAMEPAD_B
@@ -20,7 +20,7 @@ RB = vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER
 
 D_UP = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_UP
 D_DOWN = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_DOWN
-D_LEFT = vg.XUSB_BUTTON.USB_GAMEPAD_DPAD_LEFT
+D_LEFT = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_LEFT
 D_RIGHT = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_RIGHT
 
 intents = discord.Intents.default()
@@ -28,69 +28,72 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+command_counter = 0
+
 @bot.event
 async def on_ready():
     print(f'{bot.user}로 로그인 완료')
-    await bot.change_presence(status = discord.Status.online, activity = discord.Game("Controller for Dakest Dungeon"))
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game("Controller for Darkest Dungeon"))
 
-"""BUTTONS"""
+@bot.event
+async def on_message(message):
+    global command_counter
 
-@bot.command(aliases = ['A', 'a'])
-async def press_a(ctx):
-    my_gamepad.Press_Btn(A_BTN)
+    if message.author == bot.user:
+        return
 
-@bot.command(aliases = ['B', 'b'])
-async def press_b(ctx):
-    my_gamepad.Press_Btn(B_BTN)
+    command_counter += 1
 
-@bot.command(aliases = ['X', 'x'])
-async def press_x(ctx):
-    my_gamepad.Press_Btn(X_BTN)
+    if command_counter == 5:
+        await execute_command(message)
+        command_counter = 0
 
-@bot.command(aliases = ['Y', 'y'])
-async def press_y(ctx):
-    my_gamepad.Press_Btn(Y_BTN)
+    await bot.process_commands(message)
 
-@bot.command(aliases = ['start'])
-async def press_start(ctx):
-    my_gamepad.Press_Btn(Start_BTN)
+async def execute_command(message):
+    content = message.content.lower()
+    if content.startswith('!a'):
+        my_gamepad.Press_Btn(A_BTN)
+    elif content.startswith('!b'):
+        my_gamepad.Press_Btn(B_BTN)
+    elif content.startswith('!x'):
+        my_gamepad.Press_Btn(X_BTN)
+    elif content.startswith('!y'):
+        my_gamepad.Press_Btn(Y_BTN)
+    elif content.startswith('!start'):
+        my_gamepad.Press_Btn(Start_BTN)
+    elif content.startswith('!back'):
+        my_gamepad.Press_Btn(Back_BTN)
+    elif content.startswith('!+u'):
+        my_gamepad.Press_Btn(D_UP)
+    elif content.startswith('!+d'):
+        my_gamepad.Press_Btn(D_DOWN)
+    elif content.startswith('!+l'):
+        my_gamepad.Press_Btn(D_LEFT)
+    elif content.startswith('!+r'):
+        my_gamepad.Press_Btn(D_RIGHT)
+    elif content.startswith('!lb'):
+        my_gamepad.Press_Btn(LB)
+    elif content.startswith('!rb'):
+        my_gamepad.Press_Btn(RB)
+    elif content.startswith('!lt'):
+        my_gamepad.Press_LT()
+    elif content.startswith('!rt'):
+        my_gamepad.Press_RT()
+    elif content.startswith('!lsl'):
+        my_gamepad.LStickL()
+    elif content.startswith('!lsr'):
+        my_gamepad.LStickR()
+    elif content.startswith('!rsu'):
+        my_gamepad.RStickU()
+    elif content.startswith('!rsd'):
+        my_gamepad.RStickD()
+    elif content.startswith('!rsl'):
+        my_gamepad.RStickL()
+    elif content.startswith('!rsr'):
+        my_gamepad.RStickR()
 
-@bot.command(aliases = ['back'])
-async def press_back(ctx):
-    my_gamepad.Press_Btn(Back_BTN)
-
-@bot.command(aliases = ['+U', '+u'])
-async def press_du(ctx):
-    my_gamepad.Press_Btn(D_UP)
-
-@bot.command(aliases = ['+D', '+d'])
-async def press_dd(ctx):
-    my_gamepad.Press_Btn(D_DOWN)
-
-@bot.command(aliases = ['+L', '+l'])
-async def press_dl(ctx):
-    my_gamepad.Press_Btn(D_LEFT)
-
-@bot.command(aliases = ['+R', '+r'])
-async def press_dr(ctx):
-    my_gamepad.Press_Btn(D_RIGHT)
-
-@bot.command(aliases = ['LB', 'lb'])
-async def press_lb(ctx):
-    my_gamepad.Press_Btn(LB)
-
-@bot.command(aliases = ['RB', 'rb'])
-async def press_rb(ctx):
-    my_gamepad.Press_Btn(RB)
-
-"""TRIGGERS"""
-
-@bot.command(aliases = ['LT', 'lt'])
-async def press_lt(ctx):
-    my_gamepad.Press_LT()
-
-@bot.command(aliases = ['RT', 'rt'])
-async def press_lt(ctx):
-    my_gamepad.Press_RT()
+    else:
+        await message.channel.send(f"{message.content}은(는) 없거나 올바르지 않은 명령입니다.")
 
 bot.run(TOKEN)
--- my_gamepad.py
+++ my_gamepad.py
@@ -33,3 +33,57 @@ def Press_RT():
     gamepad.update()
 
 
+def LStickL():
+    gamepad.left_joystick_float(x_value_float=-1.0, y_value_float=0.0)
+    gamepad.update()
+
+    time.sleep(3)
+
+    gamepad.left_joystick_float(x_value_float=0.0, y_value_float=0.0)
+    gamepad.update()
+
+
+def LStickR():
+    gamepad.left_joystick_float(x_value_float=1.0, y_value_float=0.0)
+    gamepad.update()
+
+    time.sleep(3)
+
+    gamepad.left_joystick_float(x_value_float=0.0, y_value_float=0.0)
+    gamepad.update()
+
+def RStickU():
+    gamepad.right_joystick_float(x_value_float=0.0, y_value_float=-1.0)
+    gamepad.update()
+
+    time.sleep(0.1)
+
+    gamepad.right_joystick_float(x_value_float=0.0, y_value_float=0.0)
+    gamepad.update()
+
+def RStickD():
+    gamepad.right_joystick_float(x_value_float=0.0, y_value_float=1.0)
+    gamepad.update()
+
+    time.sleep(0.1)
+
+    gamepad.right_joystick_float(x_value_float=0.0, y_value_float=0.0)
+    gamepad.update()
+
+def RStickL():
+    gamepad.right_joystick_float(x_value_float=-1.0, y_value_float=0.0)
+    gamepad.update()
+
+    time.sleep(0.1)
+
+    gamepad.right_joystick_float(x_value_float=0.0, y_value_float=0.0)
+    gamepad.update()
+
+def RStickR():
+    gamepad.right_joystick_float(x_value_float=1.0, y_value_float=0.0)
+    gamepad.update()
+
+    time.sleep(0.1)
+
+    gamepad.right_joystick_float(x_value_float=0.0, y_value_float=0.0)
+    gamepad.update()
